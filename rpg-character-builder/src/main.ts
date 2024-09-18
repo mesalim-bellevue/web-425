25ae26df3137e2d04bc121fa96aa842ee300e775
meher-un-nisa <mehersalim2@outlook.com>
2024-09-18 18:31:29 -0500

    Delete rpg-character-builder/src directory

--- rpg-character-builder/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/styles.css/app.config';
-import { AppComponent } from './app/styles.css/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
